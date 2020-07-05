500cb02632de0b0d2c5ef2cc6c3496345f750deb
salma <SalmaEmad@aucegypt.edu>
2020-07-05 16:11:36 +0200

    v1

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"this is a git testing file";
+	cout<<"Branch 1 ..........."<<endl;
+	cout<<"this is a git testing file"<<endl;
+	cout<<"branch 2........"<<endl;
+	return 0;
+}
