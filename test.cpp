406818741a7aa99e6b0d73bb2fed7e6fefbd0d77
salma <SalmaEmad@aucegypt.edu>
2020-07-05 16:20:56 +0200

    v2

--- test.cpp
+++ test.cpp
@@ -3,8 +3,6 @@ using namespace std;
 int main()
 {
 	cout<<"this is a git testing file";
-	cout<<"Branch 1 ..........."<<endl;
-	cout<<"this is a git testing file"<<endl;
-	cout<<"branch 2........"<<endl;
+	
 	return 0;
 }
